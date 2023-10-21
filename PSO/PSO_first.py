f4e7fba98a539a8e732eb3b4bc67aac3a0561f1e
ZongSing.huang <a26833765@gmail.com>
2023-10-22 00:06:52 +0800

    feat: 甘特圖

--- PSO/PSO_first.py
+++ PSO/PSO_first.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from addict import Dict
+from matplotlib.gridspec import GridSpec
 
 """
 改变问题规模时需要改动的参数：工件参数workpiece，工序数目process，机器数目machine，
@@ -249,7 +250,7 @@ if __name__ == "__main__":
         "path": {
             "rawdata": "rawdata",
             "result": "result",
-            "filename": "data_second.xlsx",
+            "filename": "data_first.xlsx",
         },
         # 演算法參數
         "alg": {"maxiter": 500, "popsize": 50, "w": 0.9, "c": (2, 2)},
@@ -367,13 +368,117 @@ if __name__ == "__main__":
     print("按照完全随机初始化的pso算法求得的最好的工艺方案：", gbest_pop)
     print(f"整个迭代过程所耗用的时间：{end - begin:.2f}s")
 
+    # 初始化
+    Tm = np.zeros(parameter["fjsp"]["machine"]["size"])  # 每一機台的結束時間
+    To = np.zeros(
+        (parameter["fjsp"]["job"]["size"], max(parameter["fjsp"]["opra"]["seq"]))
+    )  # 每一工件每一製程的結束時間
+
+    OS = decoding_OS(particle=gbest_pop, parameter=parameter)
+    MS = decoding_MS(particle=gbest_pop, parameter=parameter)
+    gantt = list()
+    for job, opra in OS:
+        machine = MS[(job, opra)]
+        length = parameter["data"]["regular"][job][opra][machine]  # 工時
+        # 若為第一道製程，則被選中機台的結束時間直接加上工時
+        if opra == 0:
+            start = 0
+            Tm[machine] += length
+            To[job, opra] = Tm[machine]
+            end = Tm[machine]
+        # 若不為第一道製程，則機台結束時間為 max(前一製程結束時間, 被選中機台結束時間) + 工時
+        else:
+            start = max(To[job, opra - 1], Tm[machine])
+            Tm[machine] = max(To[job, opra - 1], Tm[machine]) + length
+            To[job, opra] = Tm[machine]
+            end = Tm[machine]
+        gantt.append({"Job": job, "Machine": machine, "Start": start, "End": end})
+    gantt = pd.DataFrame(gantt)
+
+    colors = [
+        "#1f77b4",
+        "#ff7f0e",
+        "#2ca02c",
+        "#d62728",
+        "#9467bd",
+        "#8c564b",
+        "#e377c2",
+        "#7f7f7f",
+        "#bcbd22",
+        "#17becf",
+        "#393b79",
+        "#5254a3",
+        "#6b6ecf",
+        "#9c9ede",
+        "#637939",
+        "#8ca252",
+        "#b5cf6b",
+        "#cedb9c",
+        "#8c6d31",
+        "#bd9e39",
+        "#e7ba52",
+        "#e7cb94",
+        "#843c39",
+        "#ad494a",
+        "#d6616b",
+        "#e7969c",
+        "#7b4173",
+        "#a55194",
+        "#ce6dbd",
+        "#de9ed6",
+        "#7f3b08",
+        "#b35806",
+        "#e08214",
+        "#fdb863",
+        "#843c39",
+        "#d9d9d9",
+        "#c6dbef",
+        "#9ecae1",
+        "#6baed6",
+        "#4292c6",
+        "#08519c",
+        "#9ecae1",
+        "#6baed6",
+        "#4292c6",
+        "#2171b5",
+        "#084594",
+        "#d9d9d9",
+        "#c6dbef",
+        "#9ecae1",
+        "#6baed6",
+    ]
+
+    sorted_labels = sorted(gantt["Job"].unique())
+    slected_colors = np.random.choice(colors, len(sorted_labels), replace=False)
+    color_dict = {Job: Color for Job, Color in zip(sorted_labels, slected_colors)}
+
     # 圖片打印
     plt.rcParams["font.sans-serif"] = ["Microsoft JhengHei"]  # 設定字體，避免中文字亂碼
-    fig = plt.figure(figsize=(10, 5))
-    ax1 = fig.add_subplot(121)
+    gs = GridSpec(2, 2)
+    ax1 = plt.subplot(gs[0, 0])
     ax1.set_title("全局最优解的变化情况")
     ax1.plot(pso_base)
-    ax2 = fig.add_subplot(122)
+    ax2 = plt.subplot(gs[0, 1])
     ax2.set_title("每次迭代后种群适应度最小值的变化情况")
     ax2.plot(iter_process)
+    ax3 = plt.subplot(gs[1, :])
+    ax3.set_title("甘特圖")
+    for index, row in gantt.iterrows():
+        ax3.barh(
+            row["Machine"],
+            width=row["End"] - row["Start"],
+            left=row["Start"],
+            color=color_dict.get(row["Job"], "gray"),
+        )
+    ax3.set_yticks(gantt["Machine"])
+    ax3.set_yticklabels(gantt["Machine"])
+    ax3.set_ylabel("Machine")
+    ax3.set_xlabel("Timeline")
+    handles = [
+        plt.Rectangle((0, 0), 1, 1, fc=color_dict.get(Job, "gray"))
+        for Job in sorted_labels
+    ]
+    ax3.legend(
+        handles, sorted_labels, title="Job", loc="center left", bbox_to_anchor=(1, 0.5)
+    )
     plt.show()
